77b8f589a2fa538bd7d9b169863cc1fee6226eeb
mamirov <m.amirov@gmail.com>
2024-11-02 17:03:35 +0400

    add base

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,13 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use wstunnel::tunnel::client::WsClient;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello, {}!", name)
+}
+
+fn main() {
+  app_lib::run();
+}
